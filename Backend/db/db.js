3563d5a37aaba0b25a39e66d1d5b1486a4bb47e5
Alekhya Ghosh <officialalekhya2004@gmail.com>
2026-09-13 13:00:27 +0530

    Remove debug log for database connection string

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 
 const ConnDB=async()=> {
     try {
-        console.log(`${process.env.DB_Connection}/${process.env.MONGO_NAME}`);
         await mongoose.connect(`${process.env.DB_Connection}/${process.env.MONGO_NAME}`)
         console.log("MongoDB Connected");
         
